84270fcb6602e3356f64afd6609c56dfd1af1bdf
ro851rud <ro851rud@htwg-konstanz.de>
2018-12-04 13:00:03 +0100

    init

---
+++ insert.sql
@@ -0,0 +1,15 @@
+
+INSERT into Touristenstraktion values ('schoen', 'Europapark');
+INSERT into Ausstattung values ('basic');
+INSERT into Ausstattung values ('fancy');
+INSERT into Ausstattung values ('president');
+INSERT into Land values ('Germany');
+INSERT into Land values ('USA');
+INSERT into Land values ('Greec');
+INSERT into Bild values ('name1', 'whonunghallo');
+INSERT into Adresse values ('Konstanz', 78467, 'Wollmatingerstr.', 41, 49);
+INSERT into Kunde values ('DE01234567890123456789', 'helo9', 'Horst', 'horst.morgen@morgen.com', 'Germany', 49);
+INSERT into Fehrienwohnung values (55, 'wohnunghallo', 50, 3, 'name1', 'Germany', 49);
+INSERT into Buchung values (to_Date('11.11.2011','DD.MM.YYYY'),to_Date('11.11.2011','DD.MM.YYYY'), 12, 'kbestaetigt', 'wohnunghallo', 2, to_Date('11.11.2011','DD.MM.YYYY'),to_Date('11.11.2011','DD.MM.YYYY'), 1, 50);
+
+  
